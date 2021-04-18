b45f6f17b8c01c52a4e60f3057b8ee632b64de16
RutB <42755206+RutB@users.noreply.github.com>
2021-04-18 21:41:43 +0000

    dummy farið :D

--- AutograderSkeleton/srcDummy/InvalidParameterException.java
+++
@@ -1,8 +0,0 @@
-public class InvalidParameterException extends RuntimeException {
-
-	private static final long serialVersionUID = 1L;
-
-	public InvalidParameterException(String message) {
-        super(message);
-    }
-}
